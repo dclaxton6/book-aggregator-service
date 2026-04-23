5f7b5c8c744ce3c6478a3af323f1b75cf389d917
dclaxton6 <daclaxton6@gmail.com>
2026-04-22 21:21:07 -0400

    test: ensure Jackson handles unknown properties and verify deep immutability in dtos

---
+++ src/main/java/com/dc/bookaggregator/BookAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.dc.bookaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookAggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dc/bookaggregator/BookAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.bookaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
